83fcc76bd8d6c54226c66492b75029776896efe2
wo-o29 <woogur29@gmail.com>
2024-11-20 02:00:09 +0900

    refactor: 라우터 수정

--- src/App.js
+++ src/App.js
@@ -6,39 +6,33 @@ export default function App() {
       <h1>메인 컴포넌트</h1>
       <div style={{display: 'flex', gap: '100px'}}>
         <Suspense fallback={<div>로딩중...</div>}>
-          <A userId={1} />
+          <TodoComponents userId={1} />
         </Suspense>
-        <B />
+        <Suspense fallback={<div>로딩중...</div>}>
+          <TodoComponents userId={2} />
+        </Suspense>
       </div>
     </div>
   );
 }
 
 const fetchUser = async (userId) => {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/todos/${userId}`
   );
   const data = await res.json();
-  console.log(data);
   return data;
 };
 
-function A({userId}) {
+function TodoComponents({userId}) {
   const todoData = use(fetchUser(userId));
-  return (
-    <div>
-      <h2>A 컴포넌트</h2>
-      <p>유저ID: {todoData.userId}</p>
-      <p>제목: {todoData.title}</p>
-    </div>
-  );
-}
 
-function B() {
   return (
     <div>
-      <h2>B 컴포넌트</h2>
+      <h3>Todo 컴포넌트</h3>
+      <p>유저ID: {todoData.id}</p>
+      <p>제목: {todoData.title}</p>
     </div>
   );
 }
--- src/framework/router.js
+++ src/framework/router.js
@@ -12,60 +12,26 @@
 // Note: this code would usually be provided by a framework.
 // ---------------------------------------------------------
 
+import {createContext, startTransition, useContext, useState, use} from 'react';
 import {
-  createContext,
-  startTransition,
-  useContext,
-  useState,
-  use,
-} from 'react';
-import {createFromFetch, createFromReadableStream} from 'react-server-dom-webpack/client';
+  createFromFetch,
+  createFromReadableStream,
+} from 'react-server-dom-webpack/client';
 
 const RouterContext = createContext();
 const initialCache = new Map();
 
 export function Router() {
   const [cache, setCache] = useState(initialCache);
-  const [location, setLocation] = useState({
-    selectedId: null,
-    isEditing: false,
-    searchText: '',
-  });
 
-  const locationKey = JSON.stringify(location);
-  let content = cache.get(locationKey);
+  let content = cache.get('r');
   if (!content) {
-    content = createFromFetch(
-      fetch('/react?location=' + encodeURIComponent(locationKey))
-    );
-    cache.set(locationKey, content);
-  }
-
-  function refresh(response) {
-    startTransition(() => {
-      const nextCache = new Map();
-      if (response != null) {
-        const locationKey = response.headers.get('X-Location');
-        const nextLocation = JSON.parse(locationKey);
-        const nextContent = createFromReadableStream(response.body);
-        nextCache.set(locationKey, nextContent);
-        navigate(nextLocation);
-      }
-      setCache(nextCache);
-    })
-  }
-
-  function navigate(nextLocation) {
-    startTransition(() => {
-      setLocation(loc => ({
-        ...loc,
-        ...nextLocation
-      }));
-    });
+    content = createFromFetch(fetch('/react'));
+    cache.set('r', content);
   }
 
   return (
-    <RouterContext.Provider value={{location, navigate, refresh}}>
+    <RouterContext.Provider value={cache}>
       {use(content)}
     </RouterContext.Provider>
   );
@@ -73,44 +39,4 @@ export function Router() {
 
 export function useRouter() {
   return useContext(RouterContext);
-}
-
-export function useMutation({endpoint, method}) {
-  const {refresh} = useRouter();
-  const [isSaving, setIsSaving] = useState(false);
-  const [didError, setDidError] = useState(false);
-  const [error, setError] = useState(null);
-  if (didError) {
-    // Let the nearest error boundary handle errors while saving.
-    throw error;
-  }
-
-  async function performMutation(payload, requestedLocation) {
-    setIsSaving(true);
-    try {
-      const response = await fetch(
-        `${endpoint}?location=${encodeURIComponent(
-          JSON.stringify(requestedLocation)
-        )}`,
-        {
-          method,
-          body: JSON.stringify(payload),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-      if (!response.ok) {
-        throw new Error(await response.text());
-      }
-      refresh(response);
-    } catch (e) {
-      setDidError(true);
-      setError(e);
-    } finally {
-      setIsSaving(false);
-    }
-  }
-
-  return [isSaving, performMutation];
 }
